e430c4dc3bd2a663e0254c00c215f1c61e4d6057
sosilion <sosilion@gmail.com>
2020-01-06 20:42:17 +0900

    practice(array.map, propTypes)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+function Food({ name, weight }) {
+  return (
+    <div>
+      {name} : {weight}kg
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  weight: PropTypes.number.isRequired
+};
+
+const people = [
+  {
+    id: 1,
+    name: "jin",
+    weight: 74
+  },
+  {
+    id: 2,
+    name: "asdfg",
+    weight: 34
+  },
+  {
+    id: 3,
+    name: "dsg",
+    weight: 56
+  },
+  {
+    id: 4,
+    name: "sdef",
+    weight: 88
+  }
+];
+
+function callPeople(person) {
+  return <Food key={person.id} name={person.name} weight={person.weight} />;
+}
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      {people.map(person => (
+        <Food key={person.id} name={person.name} weight={person.weight} />
+      ))}
+
+      {people.map(callPeople)}
+    </div>
+  );
 }
 
 export default App;
