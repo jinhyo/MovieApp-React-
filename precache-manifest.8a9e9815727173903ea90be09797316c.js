9655067395de0e907ff5f12a64ef6fa08bb176bf
sosilion <sosilion@gmail.com>
2020-01-08 15:33:24 +0900

    Updates

---
+++ precache-manifest.8a9e9815727173903ea90be09797316c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ce491ae25e113bbb59fbce24f3b407",
+    "url": "/movie-app_react/index.html"
+  },
+  {
+    "revision": "fb55ccaa73d5b24de23c",
+    "url": "/movie-app_react/static/css/main.172d7c72.chunk.css"
+  },
+  {
+    "revision": "08f8a6a246277e8bd461",
+    "url": "/movie-app_react/static/js/2.1ea602ce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie-app_react/static/js/2.1ea602ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb55ccaa73d5b24de23c",
+    "url": "/movie-app_react/static/js/main.1eee4d7f.chunk.js"
+  },
+  {
+    "revision": "0886cff7d92d6c26c439",
+    "url": "/movie-app_react/static/js/runtime-main.467f97f0.js"
+  }
+]);
